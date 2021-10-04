b01d426dc31de33f315a6f6d182038c0b6458ba1
Mateus Nobre <mateus.nobre@NB-5540.local>
2021-10-04 00:55:22 -0300

    services setup

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,37 @@
 import { Fragment } from 'react'
-import TestUpload from './components/TestUpload'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+
+import "./assets/styles/reset.css";
+import "./assets/styles/style.css";
 
 function App() {
   return (
-    <Fragment>
-      <TestUpload></TestUpload>
-    </Fragment>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home" exact>
+          <Home />
+        </Route>
+        <Route path="/sign-in" exact>
+          <SignIn />
+        </Route>
+        <Route path="/sign-up">
+          <SignUp />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
---
+++ front/src/components/FontSizes.js
@@ -0,0 +1,11 @@
+const FontSizes = {
+    xxSmall: "8px",
+    xSmall: "12px",
+    small: "16px",
+    normal: "18px",
+    medium: "20px",
+    large: "36px",
+    xLarge: "48px",
+    xxLarge: "96px"
+}
+export default FontSizes
