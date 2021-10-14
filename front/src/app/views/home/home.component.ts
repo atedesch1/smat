e72ae62f464c0bdc9e41ad6ca37d9af4a0290ab1
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-10-14 17:47:22 -0300

    add home with search

--- front/src/app/views/home/home.component.ts
+++ front/src/app/views/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/models/post';
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  searchQuery = ''
+  // matchedPosts: Observable<Post[]> | undefined
+  matchedPosts: Post[] = []
+
+  constructor(private searchService: SearchService) { }
 
   ngOnInit(): void {
   }
+
+  async getMatchedPosts() {
+    if (this.searchQuery === '') {
+      this.matchedPosts = []
+      return
+    }
+
+    // this.searchService.search(this.searchQuery).subscribe(res => { this.matchedPosts = res })
+    this.matchedPosts = await this.searchService.search(this.searchQuery).toPromise()
+  }
+
+  // getMatchedPosts(): void {
+  //   if (this.searchQuery === '') {
+  //     this.matchedPosts = new Observable<Post[]>()
+  //     return
+  //   }
+
+  //   this.matchedPosts = this.searchService.search(this.searchQuery)
+  // }
 
 }
