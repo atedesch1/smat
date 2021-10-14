9ccdabb59ebac1e785361773ccb0a1b662903b6a
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-10-14 17:48:17 -0300

    update imports

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './views/auth/auth.component';
 import { HomeComponent } from './views/home/home.component';
+import { MainComponent } from './views/main/main.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'auth', component: AuthComponent }
+  { path: 'auth', component: AuthComponent },
+  { path: 'main', component: MainComponent }
 ];
 
 @NgModule({
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { ReactiveFormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'
 
@@ -8,21 +8,27 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './views/home/home.component'
 import { AuthComponent } from './views/auth/auth.component'
+import { LocalStorageService } from './services/local-storage.service';
+import { MatchedPostComponent } from './components/matched-post/matched-post.component';
+import { MainComponent } from './views/main/main.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AuthComponent,
+    MainComponent,
+    MatchedPostComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [LocalStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
