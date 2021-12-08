a1ad3372d5aa58cbb0d31bb79b0db53ad38516eb
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-12-08 17:11:43 -0300

    add main screen

--- front/src/app/views/main/main.component.ts
+++ front/src/app/views/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   templateUrl: './main.component.html',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
   }
