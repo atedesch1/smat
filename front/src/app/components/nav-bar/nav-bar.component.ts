9dc05095d0f1685f2436bed08a45d53ec99e0a0f
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-12-08 20:54:30 -0300

    add nav-bar

---
+++ front/src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  signIn() {
+    this.router.navigateByUrl('auth')
+  }
+
+  signUp() {
+    this.router.navigateByUrl('auth')
+  }
+
+  signOut() {
+    this.authService.signOut()
+  }
+}
