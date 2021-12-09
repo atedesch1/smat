c2f0b33cd5eef2b49878a70c22512d13c1bfb392
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-12-09 02:32:35 -0300

    add user service

---
+++ front/src/app/services/user.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Post } from '../models/post';
+import { User } from '../models/user';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private userUrl = 'api/user'
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  searchUsers(searchQuery: string) {
+    let params = new HttpParams().set('searchQuery', searchQuery)
+    return this.http.get<User[]>(`${this.userUrl}/search`, { params })
+  }
+
+  updateUser(formValue: any) {
+    return this.http.put(`${this.userUrl}`, formValue)
+  }
+
+  deleteUser() {
+    return this.http.delete(`${this.userUrl}`)
+  }
+
+  getCurrentUser() {
+    return this.http.get<User>(`${this.userUrl}/session`)
+  }
+
+  getAUser(userId: string) {
+    return this.http.get<User>(`${this.userUrl}/${userId}`)
+  }
+
+  getUserPosts(userId:string) {
+    return this.http.get<Post[]>(`${this.userUrl}/${userId}/posts`)
+  }
+}
