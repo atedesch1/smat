e5570a4c711598a29b612370dd21df9fcdbad7bb
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-12-09 04:00:11 -0300

    add edit user comp

---
+++ front/src/app/components/edit-user-form/edit-user-form.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import nationalitiesJSON  from 'src/assets/nationalities.json'
+
+@Component({
+  selector: 'app-edit-user-form',
+  templateUrl: './edit-user-form.component.html',
+  styleUrls: ['./edit-user-form.component.scss']
+})
+export class EditUserFormComponent implements OnInit {
+
+  nationalities = nationalitiesJSON.nationatilies
+
+  user!: User
+  userForm!: FormGroup
+
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(user => {
+      this.user = user
+      this.setFormGroup()
+    })
+    this.setFormGroup()
+  }
+
+  setFormGroup() {
+    this.userForm = this.fb.group({
+      file: [
+        null,
+        [
+          Validators.required,
+      ]],
+      name: [
+        this.user?.name ?? '',
+        [
+          Validators.required,
+      ]],
+      nationality: [
+        this.user?.nationality ?? '',
+        [
+          Validators.required,
+      ]],
+      bio: [
+        this.user?.bio ?? '',
+        [
+          Validators.required,
+      ]],
+    })
+  }
+
+  get file() {
+    return this.userForm.get('file')
+  }
+  get name() {
+    return this.userForm.get('name')
+  }
+  get nationality() {
+    return this.userForm.get('nationality')
+  }
+  get bio() {
+    return this.userForm.get('bio')
+  }
+
+  onFileChange(event: any) {
+    if (event?.target?.files?.length) {
+      const file = event.target.files[0];
+      this.userForm.patchValue({
+          file: file,
+      })
+    }
+  }
+
+  async submitHandler() {
+    let formValue = this.userForm.value
+
+    const formData = new FormData()
+    formData.append('file', formValue.file)
+    formData.append('name', formValue.name)
+    formData.append('nationality', formValue.nationality)
+    formData.append('bio', formValue.bio)
+
+    try {
+      this.userService.updateUser(formData).subscribe()
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
