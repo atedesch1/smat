db6af9a24e52025303b682d3f3d6289558cfb2a4
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-12-09 02:32:42 -0300

    add post service

--- front/src/app/services/post.service.ts
+++ front/src/app/services/post.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/internal/operators/map';
 import { Post } from '../models/post';
 
 @Injectable({
@@ -15,35 +16,38 @@ export class PostService {
     return this.http.post(`${this.postUrl}/create`, formData)
   }
 
-  updatePost(postId: String, formData: any) {
+  updatePost(postId: string, formData: any) {
     return this.http.put(`${this.postUrl}/${postId}`, formData)
   }
 
-  deletePost(postId: String) {
-    return this.http.delete(`${this.postUrl}/${postId}`)
+  deletePost(postId: string) {
+    return this.http.delete<void>(`${this.postUrl}/${postId}`)
   }
 
-  getMatchedPosts(searchQuery: String) {
-    return this.http.get<Post[]>(`${this.postUrl}/search/${searchQuery}`)
+  searchPosts(searchQuery: string) {
+    let params = new HttpParams().set('searchQuery', searchQuery)
+    return this.http.get<Post[]>(`${this.postUrl}/search`, { params })
   }
 
-  getAPost(postId: String) {
-    return this.http.get(`${this.postUrl}/${postId}`)
+  getAPost(postId: string) {
+    return this.http.get<Post>(`${this.postUrl}/${postId}`)
   }
 
-  getPostComments(postId: String) {
+  getPostComments(postId: string) {
     return this.http.get(`${this.postUrl}/${postId}/comments`)
   }
 
-  hasLikedPost(postId: String) {
-    return this.http.get(`${this.postUrl}/${postId}/like`)
+  hasLikedPost(postId: string) {
+    return this.http.get<string>(`${this.postUrl}/${postId}/like`).pipe(
+      map(hasLikedPost => { return hasLikedPost === 'true' })
+    )
   }
 
-  likePost(postId: String) {
+  likePost(postId: string) {
     return this.http.put(`${this.postUrl}/${postId}/like`, null)
   }
 
-  unlikePost(postId: String) {
+  unlikePost(postId: string) {
     return this.http.delete(`${this.postUrl}/${postId}/like`)
   }
 
