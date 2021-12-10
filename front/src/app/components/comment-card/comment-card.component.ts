f90d7c70f0ee419e777cdda9b8947a6b5d8c4b4f
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-12-10 02:46:37 -0300

    style and add logic to comment card

--- front/src/app/components/comment-card/comment-card.component.ts
+++ front/src/app/components/comment-card/comment-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommentService } from 'src/app/services/comment.service';
 import { Comment } from 'src/app/models/comment';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-comment-card',
@@ -12,11 +13,21 @@ export class CommentCardComponent implements OnInit {
   @Input()
   comment!: Comment
 
+  @Output() commentDeleted = new EventEmitter<string>()
+
+  isUsersComment!: boolean
+  isEditMode: boolean = false
+
   constructor(
     private commentService: CommentService,
+    private userService: UserService,
   ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(user => {
+      this.isUsersComment = this.comment?.user?.id === user.id
+    })
+  }
 
   toggleLikeComment() {
     this.commentService.hasLikedComment(this.comment.id).subscribe(hasLikedComment => {
@@ -31,8 +42,19 @@ export class CommentCardComponent implements OnInit {
     })
   }
 
+  toggleEditComment() {
+    this.isEditMode = !this.isEditMode
+  }
+
+  updateComment(comment: Comment) {
+    this.toggleEditComment()
+    this.comment = comment
+  }
+
   deleteComment() {
-    this.commentService.deleteComment(this.comment.id).subscribe()
+    this.commentService.deleteComment(this.comment.id).subscribe(res => {
+      this.commentDeleted.emit(this.comment.id as string)
+    })
     // reload page
   }
 }
